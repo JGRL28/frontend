56dba7ed357289ec965c1574d81df58e3cdcab57
Jose Rodriguez <jose28206892@gmail.com>
2024-11-28 02:51:01 -0300

    no null

--- components/ui/choose-categotry.tsx
+++ components/ui/choose-categotry.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useGetCategories } from "@/api/getProducts";
 import Link from "next/link";
+import { ResponseType } from "@/types/response";
 import { CategoryType } from "@/types/category";
 
 const ChooseCategory = () => {
-  const { result, loading } = useGetCategories();
+  const { result, loading }: ResponseType = useGetCategories();
 
   return (
     <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
